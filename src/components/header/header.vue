50c562b1df545ab97db5c18e3f9951c8cd98974e
Spanky <spanky@SpankydeMacBook-Pro.local>
2018-07-17 22:31:00 +0800

    7/17 commit

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="hearder">
-        我是header
-        <span class="icon-add_circle"></span>
+        <span>我是{{name}}</span>
+        <span class="icon-add_circle" @click="sayHello"></span>
+        <input type="checkbox" name="sex" :checked="sex=='男'">男
+        <input type="checkbox" name="sex" :checked="sex=='女'">女
+        <input type="text" v-model="yym">
+        {{yym}}
     </div>
 </template>
 
 <script>
-
+    export default {
+        name: 'Header',
+        data () {
+            var yym = '仰一鸣';
+            return {
+                yym: yym,
+                name: '仰一鸣',
+                sex: '女',
+                sayHello: function () {
+                    alert(yym);
+                }
+            };
+        }
+    };
 </script>
 
 <style>
